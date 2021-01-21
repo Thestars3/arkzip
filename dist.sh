7271197d8d0c192b9a6429ae6facd0616ed98cdd
MoorDev <kjm890215@hanmail.net>
2021-01-22 08:34:07 +0900

    Update dist.sh

--- dist.sh
+++ dist.sh
@@ -91,7 +91,7 @@ function makeControlFile(){
 	Section: utils
 	Priority: optional
 	breaks: arkzip
-	depends: libqt4-core(>=4:4.8.1), libstdc++6
+	depends: libqtcore4, libstdc++6
 	Architecture: amd64
 	Maintainer: 별님 <w7dn1ng75r@gmail.com>
 	Homepage: https://github.com/Thestars3/arkzip/
